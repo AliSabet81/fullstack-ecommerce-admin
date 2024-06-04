9332db9909604b11e18bde43bf47bc0f2727c022
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-04 16:04:55 +0330

    delete modal component

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import { StoreModal } from "@/components/modals/store-modal";
+import { StoreModal } from "@/components/modals/storeModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
