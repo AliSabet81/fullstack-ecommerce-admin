39139ec0a8c97e15d15589f9904205da78eaba92
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-07 14:06:17 +0330

    get single billboard api route

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -2,6 +2,28 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_Get]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
