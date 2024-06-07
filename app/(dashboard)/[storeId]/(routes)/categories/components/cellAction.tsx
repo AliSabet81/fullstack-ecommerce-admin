2f5124334ceba038430dbf6f95a8bd6508053db2
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-07 17:55:28 +0330

    fixing category cellAction

--- app/(dashboard)/[storeId]/(routes)/categories/components/cellAction.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cellAction.tsx
@@ -29,18 +29,18 @@ export const CellAction: FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard id copied to the clipboard");
+    toast.success("Category id copied to the clipboard");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
       router.refresh();
-      toast.success("Billboard deleted");
+      toast.success("Category deleted");
     } catch (error) {
       toast.error(
-        "Make sure to removed all categories using this billboard first."
+        "Make sure to removed all products using this category first."
       );
     } finally {
       setLoading(false);
@@ -71,7 +71,7 @@ export const CellAction: FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
