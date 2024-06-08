a8d0ded27b6d777171f59cf6a0292fc861ada73f
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-08 11:05:17 +0330

    get single size route

---
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { sizeId: string } }
+) {
+  try {
+    if (!params.sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+
+    const size = await prismadb.size.findUnique({
+      where: {
+        id: params.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
