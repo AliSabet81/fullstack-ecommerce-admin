7407b9bfef1a56149cccebbf4fe4c1a6ec25237a
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-08 11:50:25 +0330

    get single color route

---
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
