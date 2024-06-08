e3d47f9054433259d55c1129f8ad892362ec58f2
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-08 11:03:59 +0330

    get sizes route

--- app/api/[storeId]/sizes/route.ts
+++ app/api/[storeId]/sizes/route.ts
@@ -63,15 +63,15 @@ export async function GET(
       return new NextResponse("Store Id is required", { status: 400 });
     }
 
-    const billboards = await prismadb.billboard.findMany({
+    const sizes = await prismadb.size.findMany({
       where: {
         storeId: params.storeId,
       },
     });
 
-    return NextResponse.json(billboards);
+    return NextResponse.json(sizes);
   } catch (error) {
-    console.log("[BILLBOARDS_GET]", error);
+    console.log("[SIZES_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
