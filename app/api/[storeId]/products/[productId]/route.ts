2c65ad01b4c7df573a4223b3434bce4b172fa3f1
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-11 23:42:48 +0330

    get single product route

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,31 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse("Product id is required", { status: 400 });
+    }
+
+    const product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        images: true,
+        category: true,
+        size: true,
+        color: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
