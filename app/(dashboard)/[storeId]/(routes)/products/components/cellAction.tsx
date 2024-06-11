1229ee3894b269d2e9dfb8f912e4e60576b1a8e9
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-12 00:18:50 +0330

    product cell action

--- app/(dashboard)/[storeId]/(routes)/products/components/cellAction.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/cellAction.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC, useState } from "react";
-import { BillboardColumn } from "./columns";
+import { ProductColumn } from "./columns";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,7 +17,7 @@ import axios from "axios";
 import AlertModal from "@/components/modals/alertModal";
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: ProductColumn;
 }
 
 export const CellAction: FC<CellActionProps> = ({ data }) => {
@@ -29,19 +29,17 @@ export const CellAction: FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard id copied to the clipboard");
+    toast.success("Product id copied to the clipboard");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/products/${data.id}`);
       router.refresh();
-      toast.success("Billboard deleted");
+      toast.success("Product deleted");
     } catch (error) {
-      toast.error(
-        "Make sure to removed all categories using this billboard first."
-      );
+      toast.error("Something went wrong");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -71,7 +69,7 @@ export const CellAction: FC<CellActionProps> = ({ data }) => {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/products/${data.id}`)
             }
           >
             <Edit className="mr-2 h-4 w-4" />
