5fc61745489cf88d82272b0e41e7a86dbcfb1617
AliSabet81 <sabet.farshid1381@gmail.com>
2024-06-11 23:27:13 +0330

    create product route

---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,90 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const {
+      name,
+      images,
+      price,
+      categoryId,
+      isFeatured,
+      isArchived,
+      sizeId,
+      colorId,
+    } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthentivated", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category Id is required", { status: 400 });
+    }
+
+    if (!sizeId) {
+      return new NextResponse("Size Id is required", { status: 400 });
+    }
+
+    if (!colorId) {
+      return new NextResponse("Color Id is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const product = await prismadb.product.create({
+      data: {
+        name,
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+        price,
+        categoryId,
+        isFeatured,
+        isArchived,
+        sizeId,
+        colorId,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCTS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
